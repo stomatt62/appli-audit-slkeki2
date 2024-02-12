a54a040a00efacc1f0d10bbd5358ede851a8d7f0
matteo <matteo.hochede62@gmail.com>
2024-02-12 17:00:17 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import PageEtaples from './PageEtaples';
 import PageWimereux from './PageWimereux';
-
-function MyButton() {
-    return (
-        <div>
-            {/* Utilisez le composant Link pour rediriger vers la page correspondante */}
-            <Link to="/etaples">
-                <button>Page Etaples</button>
-            </Link>
-            <Link to="/wimereux">
-                <button>Page Wimereux</button>
-            </Link>
-        </div>
-    );
-}
+import './App.css'; // Importez votre fichier CSS pour les styles personnalisés
 
 function App() {
+    // Définition des gestionnaires d'événements pour les boutons
+    const handleEtaplesClick = () => {
+        alert('bienvenue dans audit Etaples');
+    };
+
+    const handleWimereuxClick = () => {
+        alert('Bienvenue dans audit Wimereux');
+    };
+
     return (
         <Router>
-            <div>
-                <h1>Application d'Audit</h1>
-                <MyButton />
+            <div className="app-container">
+                <h1 className="app-title">Application d'Audit</h1>
+                <div className="container">
+                    <div className="choice-box">
+                        <h2>Choisissez votre destination :</h2>
+                        <div className="options">
+                            <button onClick={handleEtaplesClick}>Étaples</button>
+                            <button onClick={handleWimereuxClick}>Wimereux</button>
+                        </div>
+                    </div>
+                </div>
                 <Routes>
                     <Route path="/etaples" element={<PageEtaples />} />
                     <Route path="/wimereux" element={<PageWimereux />} />
@@ -32,6 +36,6 @@ function App() {
     );
 }
 
-export { App, MyButton };
+export  {App};
 
 
